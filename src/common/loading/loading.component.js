3c8d3dc6d6b583b39fde93bc17e0eb2476599336
Ying Wang <ywang.botany@gmail.com>
2017-08-04 15:02:55 -0400

    fixed spinner src

--- src/common/loading/loading.component.js
+++ src/common/loading/loading.component.js
@@ -3,7 +3,7 @@
 
 angular.module('common')
 .component('loading', {
-  template: '<img src="images/spinner.svg" ng-if="$ctrl.show">',
+  template: '<img src="src/images/spinner.svg" ng-if="$ctrl.show">',
   controller: LoadingController
 });
 
